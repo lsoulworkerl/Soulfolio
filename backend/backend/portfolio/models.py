9621536770b44c00b722f14b11cbae34fb0a34dd
lsoulworkerl <prosto2002vitalik@gmail.com>
2023-01-02 00:28:06 +0200

    Create models

--- backend/backend/portfolio/models.py
+++ backend/backend/portfolio/models.py
@@ -5,11 +5,20 @@ class Introduction(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
 
+    class Meta:
+        verbose_name = 'Introduction'
+        verbose_name_plural = 'Introductions'
+
 
 class AccountLinks(models.Model):
+    social = models.CharField(max_length=50)
     link = models.URLField(max_length=200)
     image = models.ImageField(upload_to='account')
     icon = models.ImageField(upload_to='icon')
+
+    class Meta:
+        verbose_name = 'Account Link'
+        verbose_name_plural = 'Account Links'
 
 
 class Projects(models.Model):
@@ -18,7 +27,15 @@ class Projects(models.Model):
     text = models.TextField()
     image = models.ImageField(upload_to='projects')
 
+    class Meta:
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
+
 
 class Timeline(models.Model):
     date = models.DateField()
     text = models.TextField()
+
+    class Meta:
+        verbose_name = 'Timeline'
+        verbose_name_plural = 'Timelines'
