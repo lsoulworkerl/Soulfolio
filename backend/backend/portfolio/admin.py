6d7086093e3b01ce7b1e7ddd86fbdcab3a256da6
lsoulworkerl <prosto2002vitalik@gmail.com>
2023-01-02 00:27:56 +0200

    Add admin panel for models

--- backend/backend/portfolio/admin.py
+++ backend/backend/portfolio/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Introduction, AccountLinks, Projects, Timeline
 
-# Register your models here.
+
+@admin.register(Introduction)
+class IntroductionAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+@admin.register(AccountLinks)
+class AccountLinksAdmin(admin.ModelAdmin):
+    list_display = ('social',)
+
+
+@admin.register(Projects)
+class ProjectsAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+@admin.register(Timeline)
+class TimelineAdmin(admin.ModelAdmin):
+    list_display = ('date',)
