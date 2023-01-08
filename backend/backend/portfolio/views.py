4d3284df715db734cf705db6ad8fcdbafb6f0ea5
lsoulworkerl <prosto2002vitalik@gmail.com>
2023-01-08 17:54:38 +0200

    create view for main page

--- backend/backend/portfolio/views.py
+++ backend/backend/portfolio/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from .models import Introduction, AccountLinks, Projects, Timeline
+from .serializer import (IntroductionSerializer, 
+                        AccountLinksSerializer, 
+                        ProjectsSerializer, 
+                        TimelineSerializer)
+from drf_multiple_model.views import ObjectMultipleModelAPIView
+from drf_multiple_model.pagination import MultipleModelLimitOffsetPagination
 
-# Create your views here.
+class LimitPagination(MultipleModelLimitOffsetPagination):
+    default_limit = 4
+
+
+class ObjectLimitPaginationView(ObjectMultipleModelAPIView):
+    pagination_class = LimitPagination
+    querylist = (
+        {'queryset': Introduction.objects.all(), 
+        'serializer_class': IntroductionSerializer},
+
+        {'queryset': AccountLinks.objects.all(), 
+        'serializer_class': AccountLinksSerializer},
+
+        {'queryset': Projects.objects.all(), 
+        'serializer_class': ProjectsSerializer},
+
+        {'queryset': Timeline.objects.all(), 
+        'serializer_class': TimelineSerializer},
+    )
